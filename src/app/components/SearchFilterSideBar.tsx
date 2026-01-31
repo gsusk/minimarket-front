503cfa1ead3df4b468fbe80c21f716160401ddba
gsus <satehortuamercado@gmail.com>
2026-01-30 21:08:22 -0500

    anadido pagina de busqeuda con estructura basica

---
+++ src/app/components/SearchFilterSideBar.tsx
@@ -0,0 +1,12 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+export default function SearchFilterSideBar() {
+  return (
+    <Stack>
+      <Stack>
+        <Box>
+        </Box>
+      </Stack>
+    </Stack>
+  )
+}
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,15 @@
+import { Box, Grid } from "@mui/material";
+import SearchFilterSideBar from "../components/SearchFilterSideBar";
 
 export default function Search() {
   return (
-    <div>Search</div>
+    <>
+      <Grid py={4} px={5} height={"100vh"} container spacing={2}>
+        <Grid size={3}>
+          <SearchFilterSideBar></SearchFilterSideBar>
+        </Grid>
+        <Grid size={9}>sadasd</Grid>
+      </Grid>
+    </>
   )
 }
